fa587f7feb78fbf9b52c36e15f2add14df9358de
job123-s <blmwhm219@gmail.com>
2023-05-29 15:32:39 +0900

    controllerの訂正
    ファイル名、クラスを訂正

--- app/Http/Controllers/AdminActivityController.php
+++ app/Http/Controllers/AdminActivityController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdominBlogController extends Controller
+class AdminActivityController extends Controller
 {
     //
 }
--- app/Http/Controllers/AdminBlogController.php
+++ app/Http/Controllers/AdminBlogController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdominActivityController extends Controller
+class AdminBlogController extends Controller
 {
     //
 }
