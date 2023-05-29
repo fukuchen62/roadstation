180d145232773b5267ffa063e99e2a8811d2c1a8
nobinoi <icandoitifyoutry@gmail.com>
2023-05-29 15:16:46 +0900

    モデルファイル作成
    モデルファイル作成（中身はまだ）しました。
    
    小山

---
+++ app/Models/RoadStation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoadStation extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/SpecialGoods.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecialGoods extends Model
+{
+    use HasFactory;
+}
