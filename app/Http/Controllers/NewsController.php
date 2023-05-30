89e7a81ef2369f29ccbec0b463b5aa07736538e6
flyingpoppo <134627169+flyingpoppo@users.noreply.github.com>
2023-05-30 11:39:29 +0900

    news.blade.phpの編集
    news.blade.phpの編集
    
    吉村

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// DBクラスをインポートする
+use Illuminate\Support\Facades\DB;
+use App\Models\News;
 
 class NewsController extends Controller
 {
-    //
+
+    public function newsMainView(Request $request)
+    {
+        // $items = News::all();
+
+        // $data = [
+        //     'boards' => $items,
+        // ];
+
+        // return view('board.index', $data);
+
+        return view('fronts.news');
+    }
 }
