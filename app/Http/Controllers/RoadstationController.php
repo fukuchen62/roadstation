68c1bf7ddc5ea1c5dc30d8fa0dc15523758630a4
nodejs2 <mementt1144@gmail.com>
2023-05-30 11:53:57 +0900

    station_detailの編集(仮)
    仮です

--- app/Http/Controllers/RoadstationController.php
+++ app/Http/Controllers/RoadstationController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\RoadStation;
+
 
 class RoadstationController extends Controller
 {
-    //
+    public function stationMainView(Request $request)
+    {
+        // $items = News::all();
+
+        // $data = [
+        //     'boards' => $items,
+        // ];
+
+        // return view('board.index', $data);
+
+        return view('fronts.station_detail');
+    }
 }
