b8953d729e27bd1133e395c517d8a0db6fae6b55
nobinoi <icandoitifyoutry@gmail.com>
2023-05-30 16:34:13 +0900

    ブログ関係のモデル、コントローラ、ビューファイルを作成しました。

---
+++ resources/views/fronts/blog_list.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.layout_front')
+
+@section('title', 'ブログ記事一覧')
+
+@section('content')
+
+
+
+
+    @foreach ($blogs as $blog)
+        <div>
+            <h2>{{ $blog->title }}</h2>
+            <p>カテゴリー：{{ $blog->category_id }}</p>
+            <p>内 容：{{ $blog->content }}</p>
+            {{--  <p>画像：{{ 画像ファイル }}</p>  --}}
+            <p>登録日時：{{ $blog->created_at }}</p>
+        </div>
+    @endforeach
+
+@endsection
