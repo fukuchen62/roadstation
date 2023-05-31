c6ddc90809e10de843d44bc52f9357acbd9bebee
nodejs2 <mementt1144@gmail.com>
2023-05-31 15:12:58 +0900

    マイグレーションの変更
    単数形に直しました

--- database/migrations/2023_05_29_145414_create_blogs_categories.php
+++ database/migrations/2023_05_29_145414_create_blogs_categories.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs_categories', function (Blueprint $table) {
+        Schema::create('blog_categories', function (Blueprint $table) {
             $table->integer('id');
             $table->string('category_name', 10);
         });
@@ -21,6 +21,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('blogs_categories');
+        Schema::dropIfExists('blog_categories');
     }
 };
