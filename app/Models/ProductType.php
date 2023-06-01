23ef62ee50d72c9588572b57667d05233894cbae
job123-s <blmwhm219@gmail.com>
2023-06-01 16:02:30 +0900

    6月1日作成分
    特産品
    杉本

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ProductType extends Model
 {
     use HasFactory;
+
+    /**
+     * リレーション
+     *
+     * @return void
+     */
+    public function goodsCategory()
+    {
+        return $this->belongsTo('App\Models\GoodsCategory');
+    }
+
+    public function getCategoryName()
+    {
+        return $this->goodsCategory->category_name;
+    }
 }
