0017efa014f30b0ad2de09dca16519972575ab7f
nodejs2 <mementt1144@gmail.com>
2023-06-02 11:48:54 +0900

    道の駅一覧 sry
    仮

--- resources/views/fronts/station_list.blade.php
+++ resources/views/fronts/station_list.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.layout_front')
 
 @section('description', '一覧')
+@php
+    $dsn = 'mysql:dbname=roadstation;host=localhost;charset=utf8';
+    $user = 'root';
+    $pass = '';
+    try {
+        $dbh = new PDO($dsn, $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = 'SELECT * FROM road_stations ';
+        $sql .= 'INNER JOIN areas ';
+        $sql .= 'ON road_stations.area_id = areas.id ';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = $row;
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        die();
+    } finally {
+        $dbh = null;
+        // finallyは絶対最後に起動
+        // nullでデーターベースとの接続情報を初期化
+    }
+@endphp
 
 @section('keywords', 'キーワード1,キーワード2・・・')
 
@@ -20,8 +44,19 @@
         <img src="{{ asset('/storage/imgs/road_side_2.jpg') }}" alt="man">
     </div>
     <p>道の駅一覧</p>
-    @foreach ($road_stations as $road)
-    @endforeach
+    <p>西部</p>
+    <?php foreach ($data as $key => $value) { ?>
+    <?php
+    if ($value['area_name'] === '西部') { ?>
+    <div>
+        <img src="{{ asset('/storage/imgs/' . $value['picture1']) }}" alt="">
+        <p><?= $value['station_name'] ?></p>
+    </div>
+    <?php }?>
+
+    <?php } ?>
+
+
 @endsection
 
 {{-- 該当ページ専用JS --}}
