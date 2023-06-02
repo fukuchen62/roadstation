c1e6a06b0dd4a9027404b8a4ec2e2b63731c4630
nodejs2 <mementt1144@gmail.com>
2023-06-02 11:01:44 +0900

    道の駅一覧の変更
    道の駅を変更しました

--- resources/views/fronts/station_list.blade.php
+++ resources/views/fronts/station_list.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.layout_front')
 
+@section('description', '一覧')
+
+@section('keywords', 'キーワード1,キーワード2・・・')
+
 {{-- タイトル --}}
-@section('title', 'くるくるなると')
+@section('title', '一覧')
+
+@section('pageCss')
+    <link rel="stylesheet" href="{{ asset('assets/css/fstyle_station_detail.css') }}">
+@endsection
+
+@section('key_visual')
+    <img class="sub-keyvisual" src="{{ asset('assets/images/sub-keyvisual.jpg') }}" alt="サブキービジュアル">
+@endsection
 
 @section('content')
+    <div class="car">
+        <img src="{{ asset('/storage/imgs/road_side_2.jpg') }}" alt="man">
+    </div>
+    <p>道の駅一覧</p>
+    @foreach ($road_stations as $road)
+    @endforeach
+@endsection
+
+{{-- 該当ページ専用JS --}}
+@section('pageJs')
+
+@endsection
