08a35bb8059aab5d9a3487198eaa316bb244d1af
nobinoi <134352992+nobinoi@users.noreply.github.com>
2023-06-07 18:00:57 +0900

    ブログ一覧の更新（cssの紐づけ）。小山

--- resources/views/fronts/blog_list.blade.php
+++ resources/views/fronts/blog_list.blade.php
@@ -19,33 +19,30 @@
 @section('content')
 
     <div class="blog-main">
+        <div class="blog-wrapper">
 
         {{-- ブログ記事一覧の表示部分 表示する記事数はコントローラ内のwhere句（paginate）で指定 --}}
         @foreach ($blogs as $blog)
             <a class="blog-card"
                 href="{{ url('blog-detail') }}?id={{ $blog->id }}&blog_category_id={{ $blog->blog_category_id }}">
                 <h2>{{ $blog->title }}</h2>
-                <img src="{{ asset('/storage/images/' . $blog->picture) }}" alt="">
+                <img class="card-img" src="{{ asset('/storage/images/' . $blog->picture) }}" alt="">
                 <p class="text">{!! $blog->content !!}</p>
                 <p>登録日時：{{ $blog['created_at']->format('Y年n月j日') }}</p>
                 <p>カテゴリー名：{{ $blog->blogCategory->getName() }}</p>
             </a>
         @endforeach
-
+                {{-- ページネーション表示部分 --}}
+                {{ $blogs->links('pagination::bootstrap-4') }}
+        </div>
         {{-- サイドバーのカテゴリー一覧の表示部分 --}}
-        <div>
+        <section class="side">
             <h3>カテゴリー一覧</h3>
-            <ul>
                 <ul>
                     @foreach ($blog_categories as $category)
                         <li><a href="{{ url('blog') }}?id={{ $category->id }}">{{ $category->category_name }}</a></li>
                     @endforeach
                 </ul>
-            </ul>
-        </div>
-
-        {{-- ページネーション表示部分 --}}
-        {{ $blogs->links('pagination::bootstrap-4') }}
-
+        </section>
     </div>
 @endsection
