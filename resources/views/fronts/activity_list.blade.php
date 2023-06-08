d4894b8577e145f6c188c999d554c8c2fd8e84a3
nobinoi <134352992+nobinoi@users.noreply.github.com>
2023-06-09 02:23:10 +0900

    アクティビティのヴューの更新。小山

--- resources/views/fronts/activity_list.blade.php
+++ resources/views/fronts/activity_list.blade.php
@@ -39,21 +39,23 @@
                     <h3 class="activity-name">
                         {{ $activity->activity_name }}
                     </h3>
-                    {!! $activity->discription !!}
+                    <h4 class="headline-text">営業時間：</h4> 
+                    <p class="business-text">
+                        {{ $activity->business_hours }}
+                    </p>
+                    <h4 class="headline text">料 金：</h4> 
+                    <p class="price-text">
+                        {{ $activity->price }}
+                    </p>
+                    
                 </div>
             </div>
-            <h4 class="headline-text">営業時間：</h4> 
-            <p class="business-text">
-                {{ $activity->business_hours }}
-            </p>
-            <h4 class="headline text">料 金：</h4> 
-            <p class="price-text">
-                {{ $activity->price }}
-            </p>
-            {{--  <h4 class="headline text">利用条件：</h4>  --}}
+            <h4 class="headline text">利用条件：</h4> 
             {!! $activity->conditions !!}
+            <h4 class="headline text">体験・詳細：</h4>
+            {!! $activity->discription !!}
 
-            {{--  体験したブログ記事の表示部分  --}}
+            {{-- 体験したブログ記事の表示部分  --}}
             <h3 class="headline activity-place">体験したブログ記事</h3>
             {{-- <ul class="roadstation-frame">
                         <li class="roadstation-frame"><a
