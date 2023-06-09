09967e1289ec74a4ee31eb9c04733c9befc032f0
nobinoi <icandoitifyoutry@gmail.com>
2023-06-09 11:49:24 +0900

    モデルの更新とブログの一覧画面作成

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,7 +9,21 @@ class Blog extends Model
 {
     use HasFactory;
 
-
+protected $guarded = array('id');
+    public static $rules = array(
+        'category_id' => 'required|integer',
+        'title'       => 'required|string|max:50',
+        'overview'    => 'max:200',
+        'thumbnail'   => 'required|string|max:200',
+        'is_show'     => 'required|boolean'
+    );
+    // 日本語エラーメッセージ
+    public static $messages = [
+        'category_id.required' => 'カテゴリーIDは必ず入力してください。',
+        'title.required'       => 'タイトルは必ず入力してください。',
+        'thumbnail.required'   => 'サムネ画像は必ず入力してください。',
+        'is_show.required'     => '表示フラグは必ず入力してください。'
+    ];
 
 
 
