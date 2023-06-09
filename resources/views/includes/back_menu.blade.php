e28a11d6c1cc57eb10890421fd1d8cfdd24e49d2
flyingpoppo <134627169+flyingpoppo@users.noreply.github.com>
2023-06-09 22:24:26 +0900

    back_footerの編集
    back_footerの編集
    
    吉村

--- resources/views/includes/back_menu.blade.php
+++ resources/views/includes/back_menu.blade.php
@@ -3,10 +3,10 @@
         <li>メニュー</li>
         <li><a href="{{ route('admintop') }}">ダッシュボード</a></li>
         <li><a href="{{ route('cms-newslist') }}">ニュース</a></li>
-        <li><a href="{{ route('admintop') }}">ブログ記事</a></li>
-        <li><a href="{{ route('admintop') }}">道の駅登録</a></li>
+        <li><a href="{{ route('cms-bloglist') }}">ブログ記事</a></li>
+        <li><a href="{{ route('cms-stationlist') }}">道の駅登録</a></li>
         <li><a href="{{ route('cms-typeslist') }}">特産品の種別</a></li>
         <li><a href="{{ route('cms-goodslist') }}">道の駅の特産品</a></li>
-        <li><a href="{{ route('admintop') }}">道の駅のアクティビティ</a></li>
+        <li><a href="{{ route('cms-activitylist') }}">道の駅のアクティビティ</a></li>
     </ul>
 </section>
