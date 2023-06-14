15cb78666ab7c3e36b6aad804ee17870c2e28460
okadamikako <134620077+okadamikako@users.noreply.github.com>
2023-06-14 17:02:43 +0900

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -65,13 +65,11 @@ $(function () {
             rightVal = -2000;
             $(this).removeClass("open");
 
-            // bodyに「.active」class付け外し
-            $("body").toggleClass("active");
+
         } else {
             $(this).addClass("open");
 
-            // bodyに「.active」class付け外し
-            $("body").toggleClass("active");
+
         }
         $("#global-navi").stop().animate({
             right: rightVal
