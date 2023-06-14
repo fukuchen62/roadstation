bf714e3b14729fff47146db1f5aac9620d16da0a
job123-s <blmwhm219@gmail.com>
2023-06-14 14:16:55 +0900

    stationCSS修正
    杉本

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -50,12 +50,6 @@ class ProductType extends Model
         return $items;
     }
 
-    public function getCategoryName2()
-    {
-        $items = '<p>' . $this->goodsCategory->category_name . '</p>';
-        return $items;
-    }
-
     public static function getRoadstationName($id)
     {
         $item = RoadStation::find($id);
