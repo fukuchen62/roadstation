b87b042ef3da0f8fcb01f91a33fffef5c2f02ef8
fukuchen62 <fukuchen62@gmail.com>
2023-06-14 11:15:30 +0900

    ブログ記事の一覧画面、詳細画面を修正した
    ブログ記事の一覧画面、詳細画面を修正した
    
    福島

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -20,29 +20,21 @@ class BlogController extends Controller
      */
     public function blogMainView(Request $request)
     {
-
-        $id_list = Blog::where('id')
-        ->where('is_show',1)
-        ->get();
-
         /**
          * ブログ記事（本文）の検索と取得
-         * 
+         *
          * 1.リクエストされたblogs_tableのidを検索し取得
-         * 2.$itemに取得したidのデータを代入
+         * 2.$blogに取得したidのデータを代入
          */
-        // $item = Blog::where('id', $request->id)
-        //         ->get()->first();
-
-        $item = Blog::find($request->id);
+        $blog = Blog::find($request->id);
 
         // ブログのカテゴリIDを取得
-        $blog_category_id = $item->blog_category_id;
+        $blog_category_id = $blog->blog_category_id;
 
         /**
          * 関連記事の検索と取得
          * （制作者：小山）
-         * 
+         *
          * 1.リクエスト時blog_tableのblog_category_idを検索
          * 2.リクエスト済みの本文を除外
          * 3.さらにis_showが1のデータを検索
@@ -52,10 +44,11 @@ class BlogController extends Controller
          * 7.第一引数idを$itemsに代入
          */
         $items = Blog::where('blog_category_id', $blog_category_id)
-        ->wherenot('id',$request->id)
-        ->where('is_show', 1)
-        ->orderby('created_at', 'DESC')
-        ->simplePaginate(2);
+            ->wherenot('id', $request->id)
+            ->where('is_show', 1)
+            ->orderby('created_at', 'DESC')
+            ->limit(2)
+            ->get();
         // var_dump($items);
         // dump($items);
 
@@ -63,22 +56,20 @@ class BlogController extends Controller
          * $categoryにblog_categoriesのデータをすべて代入（初期化）
          * 制作者：小山
          */
-        $category = BlogCategory::all();
-        // var_dump($category);
-
+        $categories = BlogCategory::all();
 
         /**
          * $dataにblogとcategories、blog_categoriesを代入初期化・二次元配列）
          * 制作者：小山
-         * 値$itemは記事本文（カテゴリー一覧と関連記事を除く）に使用
+         * 値$blogは記事本文（カテゴリー一覧と関連記事を除く）に使用
          * 値$itemsは関連記事の表示に使用
          * 値$categoryはカテゴリー一覧の表示に使用
          */
         $data = [
-            'blog' => $item,
-            'categories' => $items,
-            'blog_categories' => $category,
-            'id_list' => $id_list,
+            'blog' => $blog,
+            'recommend_blogs' => $items,
+            'blog_categories' => $categories,
+            // 'id_list' => $id_list,
         ];
         // var_dump($data);
 
@@ -90,10 +81,6 @@ class BlogController extends Controller
     }
 
 
-
-
-
-
     /**
      * blogListSearch function
      * ブログ記事一覧の表示
@@ -103,67 +90,99 @@ class BlogController extends Controller
      */
     public function blogListSearch(Request $request)
     {
-        
+
+        /***
+         * データベースから表示できる記事を取得
+         *
+         * 【ifelse文の解説】
+         *
+         * idと数字が同じ場合、下記を順番に行う
+         * 1.deleted_atの値が、nullのものを検索
+         * 2.blogs_tableからis_showと同じ数字のデータを検索
+         * 3.blogs_tableからblog_category_idと同じ数字のデータを検索
+         * 4.検索結果より、1と2の条件をみたすデータを取得
+         * 5.取得したデータをidの大きいものから順番に並べる
+         * 6.取得したデータが6以上ならページネーションを行う
+         */
+
+        // fukushima Mod 2023-06-14 Start
+
+        // if ($request->id == 1) {
+        //     $items = Blog::where('deleted_at', null)
+        //         ->where('is_show', 1)
+        //         ->where('blog_category_id', 1)
+        //         ->orderby('id', 'DESC')
+        //         ->Paginate(4);
+        // } elseif ($request->id == 2) {
+        //     $items = Blog::where('deleted_at', null)
+        //         ->where('is_show', 1)
+        //         ->where('blog_category_id', 2)
+        //         ->orderby('id', 'DESC')
+        //         ->Paginate(4);
+        // } elseif ($request->id == 3) {
+        //     $items = Blog::where('deleted_at', null)
+        //         ->where('is_show', 1)
+        //         ->where('blog_category_id', 3)
+        //         ->orderby('id', 'DESC')
+        //         ->Paginate(4);
+        // } else {
+        //     $items = Blog::where('deleted_at', null)
+        //         ->where('is_show', 1)
+        //         ->orderby('id', 'DESC')
+        //         ->Paginate(4);
+        // }
+
+        // カテゴリーIDを取得
+        $category_id = 0;
+        if (isset($request->category_id)) {
+            $category_id = $request->category_id;
+        }
+
+        $count = 0;
+        if ($category_id != 0) {
+            // 指定IDでブログ記事を読み込む
+            $items = Blog::where('blog_category_id', $category_id)
+                ->where('deleted_at', null)
+                ->where('is_show', 1)
+                ->orderby('id', 'DESC')
+                ->Paginate(4);
+
+            // 件数を取得
+            $count = Blog::where('blog_category_id', $category_id)
+                ->where('deleted_at', null)
+                ->where('is_show', 1)
+                ->count();
+        } else {
+            // すべての記事を読み込む
+            $items = Blog::where('deleted_at', null)
+                ->where('is_show', 1)
+                ->orderby('id', 'DESC')
+                ->Paginate(4);
+            // 件数を取得
+            $count = Blog::where('deleted_at', null)
+                ->where('is_show', 1)
+                ->count();
+        }
+        // fukushima Mod 2023-06-14 End
+
         /**
-             * データから表示可能の記事を取得
-             */
-            // $items = Blog::where('is_show',1)->get();
+         * $categoryにblog_categoriesのデータをすべて代入
+         */
+        $category = BlogCategory::all();
 
-            /***
-             * データベースから表示できる記事を取得
-             * 
-             * 【ifelse文の解説】
-             * 
-             * idと数字が同じ場合、下記を順番に行う
-             * 1.deleted_atの値が、nullのものを検索
-             * 2.blogs_tableからis_showと同じ数字のデータを検索
-             * 3.blogs_tableからblog_category_idと同じ数字のデータを検索
-             * 4.検索結果より、1と2の条件をみたすデータを取得
-             * 5.取得したデータをidの大きいものから順番に並べる
-             * 6.取得したデータが6以上ならページネーションを行う
-             */
-            if ($request->id == 1) {
-                $items = Blog::where('deleted_at', null)
-                    ->where('is_show', 1)
-                    ->where('blog_category_id', 1)
-                    ->orderby('id', 'DESC')
-                    ->Paginate(4);
-            } elseif ($request->id == 2) {
-                $items = Blog::where('deleted_at', null)
-                    ->where('is_show', 1)
-                    ->where('blog_category_id', 2)
-                    ->orderby('id', 'DESC')
-                    ->Paginate(4);
-            } elseif ($request->id == 3) {
-                $items = Blog::where('deleted_at', null)
-                    ->where('is_show', 1)
-                    ->where('blog_category_id', 3)
-                    ->orderby('id', 'DESC')
-                    ->Paginate(4);
-            } else {
-                $items = Blog::where('deleted_at', null)
-                    ->where('is_show', 1)
-                    ->orderby('id', 'DESC')
-                    ->Paginate(4);
-            }
+        /**
+         * $dataにblogとblog_categoriesを代入
+         * （二次元配列）
+         */
+        $data = [
+            'blogs' => $items,
+            'blog_categories' => $category,
+            'count' => $count,
+        ];
 
-            /**
-             * $categoryにblog_categoriesのデータをすべて代入
-             */
-            $category = BlogCategory::all();
-
-            /**
-             * $dataにblogとblog_categoriesを代入
-             * （二次元配列）
-             */
-            $data = [
-                'blogs' => $items,
-                'blog_categories' => $category,
-            ];
-
-            /**
-             * $dataをviewヘルパを使い、blog_listページに送る処理
-             */
-            return view('fronts.blog_list',$data);
+        /**
+         * $dataをviewヘルパを使い、blog_listページに送る処理
+         */
+        return view('fronts.blog_list', $data);
     }
 }
