8342efadde76779f57b540eb9dc7b6817a689cf4
nobinoi <134352992+nobinoi@users.noreply.github.com>
2023-06-17 07:56:42 +0900

    ブログ一覧の更新。小山
    html内のCSS削除。

--- resources/views/fronts/blog_list.blade.php
+++ resources/views/fronts/blog_list.blade.php
@@ -14,107 +14,15 @@
     <link rel="stylesheet" href="{{ asset('assets/css/blog_list.css') }}">
 
     <style>
-        /** カテゴリー一覧 **/
-        /* .side {
-                                                                            width: 100%;
-                                                                            height: 320px;
-                                                                            background-color: #ffffff;
-                                                                            box-shadow: 5px 5px 8px 0 #70b27f;
-                                                                            border: groove 6px #A7D28D;
-                                                                            color: #8c5243;
-                                                                            border-radius: 24px;
-                                                                            padding: 20px;
-                                                                        }
 
-                                                                        .side h3{
-                                                                            font-size: 28px;
-                                                                        }
-                                                                        .side li{
-                                                                                font-size: 24px;
-                                                                                margin-top: 24px;
-                                                                            } */
-
-        /* @media screen and (min-width:430x){
-                                                                            .side{
-                                                                                width:80%;
-                                                                                margin: auto;
-                                                                            }
-
-                                                                        } */
-
-        /* @media screen and (min-width:768px){
-                                                                            .side{
-                                                                                width:50%;
-                                                                            } */
-
-        /* @media screen and (min-width:830px){
-                                                                            .side{
-                                                                                width:100%;
-                                                                            } */
-
-        /* @media screen and (min-width:1080px){
-                                                                            .side{
-                                                                                width:25%;
-                                                                            }
-                                                                            } */
 
         /** ページネーション **/
-        .link {
-            width: 100%;
-            margin-top: 32px;
-            text-align: center;
-        }
 
-        .pagination {
-            margin: auto;
-            margin-top: 16px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .page-link {
-            display: block;
-            border-radius: 50%;
-            width: 56px;
-            height: 56px;
-            margin: auto;
-        }
-
-        .page-item:nth-child(odd) {
-            background-color: #5eb356;
-            border-radius: 50%;
-            color: #fffbf1;
-            font-size: 40px;
-            font-weight: bold;
-        }
-
-        .page-item:nth-child(even) {
-            background-color: #5eb356;
-            border-radius: 50%;
-            color: #fffbf1;
-            font-size: 40px;
-            font-weight: bold;
-        }
-
-        .page-item:nth-of-type(1) {
-            background-color: #fffbf1;
-            color: #91471d;
-            font-size: 40px;
-            font-weight: bold;
-        }
-
-        .page-item:nth-last-of-type(1) {
-            background-color: #fffbf1;
-            color: #91471d;
-            font-size: 40px;
-            font-weight: bold;
-        }
 
         @media screen and (min-width:768px) {
             .pagination {
                 /* width: 51%; */
-                width: 50%;
+                /* width: 50%; */
             }
         }
 
@@ -122,34 +30,14 @@
             nav {
                 /* width: 790px; */
                 /* width: 71%; */
-                margin: auto;
+                /* margin: auto; */
             }
 
             /* .pagination{
-                                                                                width: 51%;
-                                                                            } */
+                width: 51%;
+            } */
 
-            .page-link {
-                width: 44px;
-                height: 44px;
-            }
 
-            .page-item:nth-child(odd) {
-                font-size: 32px;
-            }
-
-            .page-item:nth-child(even) {
-                font-size: 32px;
-            }
-
-            .page-item:nth-of-type(1) {
-                font-size: 32px;
-            }
-
-            .page-item:nth-last-of-type(1) {
-                font-size: 32px;
-            }
-        }
     </style>
 @endsection
 
