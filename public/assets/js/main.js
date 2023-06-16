7caa5532fff56172c2759543bb748f76f60f2f1c
okadamikako <134620077+okadamikako@users.noreply.github.com>
2023-06-17 01:23:19 +0900

    TOPページ修正
    TOPページ修正
    
    岡田

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -89,3 +89,72 @@ function doReload(url) {
     // window.location.reload();
     window.location.href = url;
 }
+
+
+// アニメーションフェードイン
+$(function () {
+    const fade_bottom = 200; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 500; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".scroll-fade-up").css({
+        opacity: 0,
+        transform: "translateY(" + fade_move + "px)",
+        transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+        const scroll_top = $(this).scrollTop();
+        const scroll_bottom = scroll_top + $(this).height();
+        const fade_position = scroll_bottom - fade_bottom;
+        $(".scroll-fade-up").each(function () {
+            const this_position = $(this).offset().top;
+            if (fade_position > this_position) {
+                $(this).css({
+                    opacity: 1,
+                    transform: "translateY(0)",
+                });
+            }
+        });
+    });
+});
+
+// 左右からフェードイン
+$(function () {
+    const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+    const fade_move = 100; // どのぐらい要素を動かすか(px)
+    const fade_time = 800; // フェードの時間(ms)
+    // フェード前のcssを定義
+    $(".scroll-fade-side").css({
+        opacity: 0,
+        transform: "translateX(" + fade_move + "px)",
+        transition: fade_time + "ms",
+    });
+    // スクロールまたはロードするたびに実行
+    $(window).on("scroll load", function () {
+        const scroll_top = $(this).scrollTop();
+        const scroll_bottom = scroll_top + $(this).height();
+        const fade_position = scroll_bottom - fade_bottom;
+        $(".scroll-fade-side").each(function () {
+            const this_position = $(this).offset().top;
+            if (fade_position > this_position) {
+                $(this).css({
+                    opacity: 1,
+                    transform: "translateX(0)",
+                });
+            }
+        });
+    });
+});
+
+
+// フッターガタゴトアニメ
+$(document).ready(function () {
+    $('.demo_wrap').on('animationend', function () {
+        if ($(this).attr('data-order') === 'left') {
+            $(this).attr('data-order', 'right');
+        } else {
+            $(this).attr('data-order', 'left');
+        }
+    });
+});
