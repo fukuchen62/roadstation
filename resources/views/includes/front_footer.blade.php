c5e9d52c7ecfe3c566e018a6c0e73fb2d24d935d
saikamaiko <mayizizahe@gmail.com>
2023-10-15 16:04:11 +0900

    Update front_footer.blade.php

--- resources/views/includes/front_footer.blade.php
+++ resources/views/includes/front_footer.blade.php
@@ -28,18 +28,22 @@
             <ul class="footer-menu-list">
 
                 <li><a href="{{ route('stationlist') }}">
-                        <img src="{{ asset('assets/images/menu_icon/station_list_icon_w.svg') }}" alt="道の駅一覧" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/station_list_icon_w.svg') }}" alt="道の駅一覧"
+                            width="30" height="20">
                         道の駅一覧</a></li>
 
                 <li><a href="{{ route('areasearch') }}">
-                        <img src="{{ asset('assets/images/menu_icon/station_area_icon_w.svg') }}" alt="エリア検索" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/station_area_icon_w.svg') }}" alt="エリア検索"
+                            width="30" height="20">
                         エリア検索</a></li>
 
                 <li><a href="{{ route('ditailsearch') }}">
-                        <img src="{{ asset('assets/images/menu_icon/station_detail_icon_w.svg') }}" alt="詳細検索" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/station_detail_icon_w.svg') }}" alt="詳細検索"
+                            width="30" height="20">
                         詳細検索</a></li>
                 <li><a href="{{ route('news') }}">
-                        <img src="{{ asset('assets/images/menu_icon/news_icon_w.svg') }}" alt="ニュース" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/news_icon_w.svg') }}" alt="ニュース" width="30"
+                            height="20">
                         ニュース</a></li>
 
             </ul>
@@ -47,41 +51,49 @@
             <ul class="footer-menu-list">
 
                 <li><a href="{{ route('goods') }}">
-                        <img src="{{ asset('assets/images/menu_icon/goods_list_icon_w.svg') }}" alt="特産品" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/goods_list_icon_w.svg') }}" alt="特産品"
+                            width="30" height="20">
                         特産品</a></li>
 
                 <li><a href="{{ route('activity') }}">
-                        <img src="{{ asset('assets/images/menu_icon/activity_list_icon_w.svg') }}" alt="体験" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/activity_list_icon_w.svg') }}" alt="体験"
+                            width="30" height="20">
                         体験</a></li>
 
                 <li><a href="{{ route('bloglist') }}">
-                        <img src="{{ asset('assets/images/menu_icon/blog_list_icon_w.svg') }}" alt="ブログ" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/blog_list_icon_w.svg') }}" alt="ブログ"
+                            width="30" height="20">
                         ブログ</a></li>
 
                 <li>
                     <a href="{{ route('mypage') }}">
-                        <img src="{{ asset('assets/images/menu_icon/like_icon_w.svg') }}" alt="お気に入り" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/like_icon_w.svg') }}" alt="お気に入り" width="30"
+                            height="20">
                         お気に入り</a>
                 </li>
             </ul>
 
             <ul>
                 <li><a href="{{ route('contact') }}">
-                        <img src="{{ asset('assets/images/menu_icon/contact_icon_w.svg') }}" alt="お問い合わせ" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/contact_icon_w.svg') }}" alt="お問い合わせ"
+                            width="30" height="20">
                         お問い合わせ</a></li>
                 <li><a href="{{ route('about') }}">
-                        <img src="{{ asset('assets/images/menu_icon/about-this-website_icon_w.svg') }}" alt="このサイトについて" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/about-this-website_icon_w.svg') }}" alt="このサイトについて"
+                            width="30" height="20">
                         このサイトについて</a></li>
                 <li><a href="{{ route('privacy') }}">
-                        <img src="{{ asset('assets/images/menu_icon/privacy-policy_icon_w.svg') }}" alt="プライバシーポリシー" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/privacy-policy_icon_w.svg') }}" alt="プライバシーポリシー"
+                            width="30" height="20">
                         プライバシーポリシー</a></li>
                 <li><a href="{{ route('admintop') }}" target="_blank">
-                        <img src="{{ asset('assets/images/menu_icon/login-page_icon_w.svg') }}" alt="ログインページ" width="30" height="20">
+                        <img src="{{ asset('assets/images/menu_icon/login-page_icon_w.svg') }}" alt="ログインページ"
+                            width="30" height="20">
                         ログインページ</a></li>
             </ul>
 
         </div>
 
-        <p class="copyright">Copyright@awa-roadstation.com</p>
+        <p class="copyright">&copy;とくしま まるっと！道の駅　制作委員会</p>
 
     </footer>
