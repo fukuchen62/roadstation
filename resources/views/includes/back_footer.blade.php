a61d0b696011120e8a6ff66e4c74b6c3d2be3a11
saikamaiko <mayizizahe@gmail.com>
2023-10-15 16:00:42 +0900

    Update back_footer.blade.php

--- resources/views/includes/back_footer.blade.php
+++ resources/views/includes/back_footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="">
     <div class="footer container">
-        <p class="copyright ta-c">Copyright@awa-roadstation.com</p>
+        <p class="copyright ta-c">&copy;とくしま　まるっと！道の駅　制作委員会</p>
     </div>
 </footer>
